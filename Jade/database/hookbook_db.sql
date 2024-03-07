a8bd6fde220069defea03694a74890ca08ddf4f0
cosma101 <161182568+cosma101@users.noreply.github.com>
2024-03-07 13:43:07 +0800

    Add files via upload

---
+++ Jade/database/hookbook_db.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2024 at 06:41 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hookbook_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_list`
+--
+
+CREATE TABLE `booking_list` (
+  `id` int(30) NOT NULL,
+  `ref_code` varchar(100) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `facility_id` int(30) NOT NULL,
+  `date_from` date NOT NULL,
+  `date_to` date NOT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '0 = Pending\r\n1 = Confirmed,\r\n2 = Done,\r\n3= Cancelled',
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_list`
+--
+
+CREATE TABLE `client_list` (
+  `id` int(30) NOT NULL,
+  `firstname` text NOT NULL,
+  `middlename` text DEFAULT NULL,
+  `username` text NOT NULL,
+  `gender` text NOT NULL,
+  `contact` text NOT NULL,
+  `address` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_added` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facility_list`
+--
+
+CREATE TABLE `facility_list` (
+  `id` int(30) NOT NULL,
+  `facility_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking_list`
+--
+ALTER TABLE `booking_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `client_list`
+--
+ALTER TABLE `client_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`(768));
+
+--
+-- Indexes for table `facility_list`
+--
+ALTER TABLE `facility_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking_list`
+--
+ALTER TABLE `booking_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `client_list`
+--
+ALTER TABLE `client_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `facility_list`
+--
+ALTER TABLE `facility_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
